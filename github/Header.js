0027e36ca3c474ecf04fdaa5d11df6430694d096
Cheki <bom0704@naver.com>
2023-06-19 17:33:19 +0900

    Delete github directory

--- github/Header.js
+++
@@ -1,7 +0,0 @@
-export default function Header () {
-  return (
-    <header className="w-100% border-b border-black h-24 relative">
-      <h1 className='text-2xl font-semibold text-center absolute left-0 right-0 top-0 bottom-0 my-auto h-8 '>사고조회</h1>
-    </header>
-  )
-}
